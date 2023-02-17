c3abd48cb89129725548340a67c3ef9a1ec18904
RaspBri <bnmu777@yahoo.com>
2023-02-16 21:17:17 -0600

    Added 2 more tutors

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -34,6 +34,8 @@ export default function Search() {
         <SearchBar align='left' style={{marginLeft: '10%', marginTop: '3%',  maxWidth: '50%'}} placeholder='enter tutor name' />
         <TutorCard initials='JD' name='John Doe' subtitle='subtitle' desc='This is a description' subject1='MATH 2414' subject2='CS 3377'/>
         <TutorCard initials='PT' name='Paige Turner' subtitle='subtitle 2' desc='This is another description' subject1='CHEM 1301' subject2='PHYS 2325'/>
+        <TutorCard initials='MP' name='Megan Jovon Ruth Pete' subtitle ='subtitle 3' desc='This is a description' subject1='MUSI 2129' subject2='MUSI 3382'/>
+        <TutorCard initials='BC' name='Beyoncé Giselle Knowles-Carter' subtitle='subtitle 4' desc='This is another description' subject1='MATH 1306' subject2='ATCM 2343 '/>
     </div>
     
     
