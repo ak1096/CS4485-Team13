663e1f2d2b7cd9198250b385181743294a90c86f
posadari <57573681+posadari@users.noreply.github.com>
2023-05-02 21:37:37 -0500

    fix index.html

--- client/src/data/apiEndpoints.js
+++ client/src/data/apiEndpoints.js
@@ -6,11 +6,11 @@
 // export const GET_APPOINTMENTS = 'http://localhost:8080/auth/get-appointments';
 // export const CREATE_APPOINTMENTS = 'http://localhost:8080/auth/create-appointments';
 
-export const GET_FAVORITES = 'http://localhost:8080/auth/get-favorites';
-export const UPDATE_FAVORITES = 'http://localhost:8080/auth/update-favorites';
-export const STUDENT_REGISTER = 'http://localhost:8080/auth/student-register';
-export const TUTOR_REGISTER = 'http://localhost:8080/auth/tutor-register';
-export const LOGIN = 'http://localhost:8080/auth/login';
-export const GET_APPOINTMENTS = 'http://localhost:8080/auth/get-appointments';
-export const CREATE_APPOINTMENTS = 'http://localhost:8080/auth/create-appointments';
-export const GET_TUTORS = 'http://localhost:8080/tutors';
+export const GET_FAVORITES = 'https://tihkr.onrender.com/auth/get-favorites';
+export const UPDATE_FAVORITES = 'https://tihkr.onrender.com/auth/update-favorites';
+export const STUDENT_REGISTER = 'https://tihkr.onrender.com/auth/student-register';
+export const TUTOR_REGISTER = 'https://tihkr.onrender.com/auth/tutor-register';
+export const LOGIN = 'https://tihkr.onrender.com/auth/login';
+export const GET_APPOINTMENTS = 'https://tihkr.onrender.com/auth/get-appointments';
+export const CREATE_APPOINTMENTS = 'https://tihkr.onrender.com/auth/create-appointments';
+export const GET_TUTORS = 'https://tihkr.onrender.com/tutors';
